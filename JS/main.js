39b001672b030ca5eba78c17b970189963a8c070
Galethis <Galethis@outlook.com>
2026-07-15 21:07:57 -0400

    Update back link text to 'Back To Home'

--- JS/main.js
+++ JS/main.js
@@ -389,7 +389,7 @@ function launchGame(gameId) {
             </style>
           </head>
           <body>
-            <a href="https://glaxyias.github.io/" class="back-btn">← Back to Games</a>
+            <a href="https://glaxyias.github.io/" class="back-btn">← Back To Home</a>
             <iframe src="${game.jsbin}" sandbox="allow-scripts allow-same-origin allow-forms"></iframe>
           </body>
           </html>
