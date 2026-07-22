db6f015a1380cd1af4ac82ebb47d2cb308a998a4
glaxyias <Sparefornow2026@outlook.com>
2026-07-22 09:32:27 -0400

    Rewrite TEST2.sh to do nothing
    The script has been completely rewritten to serve as a 'Wall of Nothing', initializing variables and echoing blank lines without performing any meaningful operations.

--- TEST2.sh
+++ TEST2.sh
@@ -1,7 +1,277 @@
-#!/bin/sh
-# e as a service
-# usage:
-# ./eaas.sh
-# xdg-open http://127.0.0.1:3333
-while true; do
-  ( printf "HTTP/1.0 200 OK\r\n\r\n<html><body>" && yes e ) |
+#!/usr/bin/env bash
+# ==============================================================================
+# The "Wall of Nothing" Script
+# ==============================================================================
+
+echo "Initializing the void..."
+
+# The ':' command is a shell built-in that does nothing and returns success.
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+
+# Now let's assign a variable hundreds of times for absolutely no reason.
+VOID_VAR_1="Nothing to see here"
+VOID_VAR_2="Nothing to see here"
+VOID_VAR_3="Nothing to see here"
+VOID_VAR_4="Nothing to see here"
+VOID_VAR_5="Nothing to see here"
+VOID_VAR_6="Nothing to see here"
+VOID_VAR_7="Nothing to see here"
+VOID_VAR_8="Nothing to see here"
+VOID_VAR_9="Nothing to see here"
+VOID_VAR_10="Nothing to see here"
+VOID_VAR_11="Nothing to see here"
+VOID_VAR_12="Nothing to see here"
+VOID_VAR_13="Nothing to see here"
+VOID_VAR_14="Nothing to see here"
+VOID_VAR_15="Nothing to see here"
+VOID_VAR_16="Nothing to see here"
+VOID_VAR_17="Nothing to see here"
+VOID_VAR_18="Nothing to see here"
+VOID_VAR_19="Nothing to see here"
+VOID_VAR_20="Nothing to see here"
+VOID_VAR_21="Nothing to see here"
+VOID_VAR_22="Nothing to see here"
+VOID_VAR_23="Nothing to see here"
+VOID_VAR_24="Nothing to see here"
+VOID_VAR_25="Nothing to see here"
+VOID_VAR_26="Nothing to see here"
+VOID_VAR_27="Nothing to see here"
+VOID_VAR_28="Nothing to see here"
+VOID_VAR_29="Nothing to see here"
+VOID_VAR_30="Nothing to see here"
+VOID_VAR_31="Nothing to see here"
+VOID_VAR_32="Nothing to see here"
+VOID_VAR_33="Nothing to see here"
+VOID_VAR_34="Nothing to see here"
+VOID_VAR_35="Nothing to see here"
+VOID_VAR_36="Nothing to see here"
+VOID_VAR_37="Nothing to see here"
+VOID_VAR_38="Nothing to see here"
+VOID_VAR_39="Nothing to see here"
+VOID_VAR_40="Nothing to see here"
+VOID_VAR_41="Nothing to see here"
+VOID_VAR_42="Nothing to see here"
+VOID_VAR_43="Nothing to see here"
+VOID_VAR_44="Nothing to see here"
+VOID_VAR_45="Nothing to see here"
+VOID_VAR_46="Nothing to see here"
+VOID_VAR_47="Nothing to see here"
+VOID_VAR_48="Nothing to see here"
+VOID_VAR_49="Nothing to see here"
+VOID_VAR_50="Nothing to see here"
+VOID_VAR_51="Nothing to see here"
+VOID_VAR_52="Nothing to see here"
+VOID_VAR_53="Nothing to see here"
+VOID_VAR_54="Nothing to see here"
+VOID_VAR_55="Nothing to see here"
+VOID_VAR_56="Nothing to see here"
+VOID_VAR_57="Nothing to see here"
+VOID_VAR_58="Nothing to see here"
+VOID_VAR_59="Nothing to see here"
+VOID_VAR_60="Nothing to see here"
+VOID_VAR_61="Nothing to see here"
+VOID_VAR_62="Nothing to see here"
+VOID_VAR_63="Nothing to see here"
+VOID_VAR_64="Nothing to see here"
+VOID_VAR_65="Nothing to see here"
+VOID_VAR_66="Nothing to see here"
+VOID_VAR_67="Nothing to see here"
+VOID_VAR_68="Nothing to see here"
+VOID_VAR_69="Nothing to see here"
+VOID_VAR_70="Nothing to see here"
+VOID_VAR_71="Nothing to see here"
+VOID_VAR_72="Nothing to see here"
+VOID_VAR_73="Nothing to see here"
+VOID_VAR_74="Nothing to see here"
+VOID_VAR_75="Nothing to see here"
+VOID_VAR_76="Nothing to see here"
+VOID_VAR_77="Nothing to see here"
+VOID_VAR_78="Nothing to see here"
+VOID_VAR_79="Nothing to see here"
+VOID_VAR_80="Nothing to see here"
+VOID_VAR_81="Nothing to see here"
+VOID_VAR_82="Nothing to see here"
+VOID_VAR_83="Nothing to see here"
+VOID_VAR_84="Nothing to see here"
+VOID_VAR_85="Nothing to see here"
+VOID_VAR_86="Nothing to see here"
+VOID_VAR_87="Nothing to see here"
+VOID_VAR_88="Nothing to see here"
+VOID_VAR_89="Nothing to see here"
+VOID_VAR_90="Nothing to see here"
+VOID_VAR_91="Nothing to see here"
+VOID_VAR_92="Nothing to see here"
+VOID_VAR_93="Nothing to see here"
+VOID_VAR_94="Nothing to see here"
+VOID_VAR_95="Nothing to see here"
+VOID_VAR_96="Nothing to see here"
+VOID_VAR_97="Nothing to see here"
+VOID_VAR_98="Nothing to see here"
+VOID_VAR_99="Nothing to see here"
+VOID_VAR_100="Nothing to see here"
+
+# Just to make sure we are really doing nothing, let's echo blank lines
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+
+# Let's check some non-existent conditions
+if [ 1 -eq 2 ]; then
+    echo "Math is broken."
+fi
+
+if [ "apple" == "banana" ]; then
+    echo "Fruit is broken."
+fi
+
+if [ false ]; then
+    :
+fi
+
+# More useless variables
+FILLER_1="Still just filling space"
+FILLER_2="Still just filling space"
+FILLER_3="Still just filling space"
+FILLER_4="Still just filling space"
+FILLER_5="Still just filling space"
+FILLER_6="Still just filling space"
+FILLER_7="Still just filling space"
+FILLER_8="Still just filling space"
+FILLER_9="Still just filling space"
+FILLER_10="Still just filling space"
+FILLER_11="Still just filling space"
+FILLER_12="Still just filling space"
+FILLER_13="Still just filling space"
+FILLER_14="Still just filling space"
+FILLER_15="Still just filling space"
+FILLER_16="Still just filling space"
+FILLER_17="Still just filling space"
+FILLER_18="Still just filling space"
+FILLER_19="Still just filling space"
+FILLER_20="Still just filling space"
+FILLER_21="Still just filling space"
+FILLER_22="Still just filling space"
+FILLER_23="Still just filling space"
+FILLER_24="Still just filling space"
+FILLER_25="Still just filling space"
+FILLER_26="Still just filling space"
+FILLER_27="Still just filling space"
+FILLER_28="Still just filling space"
+FILLER_29="Still just filling space"
+FILLER_30="Still just filling space"
+FILLER_31="Still just filling space"
+FILLER_32="Still just filling space"
+FILLER_33="Still just filling space"
+FILLER_34="Still just filling space"
+FILLER_35="Still just filling space"
+FILLER_36="Still just filling space"
+FILLER_37="Still just filling space"
+FILLER_38="Still just filling space"
+FILLER_39="Still just filling space"
+FILLER_40="Still just filling space"
+FILLER_41="Still just filling space"
+FILLER_42="Still just filling space"
+FILLER_43="Still just filling space"
+FILLER_44="Still just filling space"
+FILLER_45="Still just filling space"
+FILLER_46="Still just filling space"
+FILLER_47="Still just filling space"
+FILLER_48="Still just filling space"
+FILLER_49="Still just filling space"
+FILLER_50="Still just filling space"
+
+# And a few more no-ops for good measure
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+:
+
+echo "Script has successfully completed doing absolutely nothing."
+exit 0
