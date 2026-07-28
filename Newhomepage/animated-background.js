83f3f56e1b13b687c75d9f0c2d3356da2fc6b331
Glaeesas <JLsniperelite4@outlook.com>
2026-07-28 08:56:05 -0400

    Reduce mouse distortion radius from 70 to 45

--- Newhomepage/animated-background.js
+++ Newhomepage/animated-background.js
@@ -20,7 +20,7 @@ class GeometricMeshBackground {
     this.mouseTrail = [];
     this.gridSize = 40;
     this.waveAmplitude = 15;
-    this.mouseDistortionRadius = 70;
+    this.mouseDistortionRadius = 45;
     this.mouseDistortionForce = 1.2;
 
     this.colors = {
