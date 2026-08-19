d9092a999f536fe583f8f0ed807985941a56ea87
Galethis <296002166+Galethis@users.noreply.github.com>
2026-08-19 12:26:46 +0000

    Auto-update game registry

--- JS/main.js
+++ JS/main.js
@@ -124,6 +124,8 @@ let _0xData = [
   { id: "jailbreakobby", title: "jailbreakobby", url: "../Games/jailbreakobby/index.html", desc: "Coming soon. Edit this description in main.js.", popular: true },
 
   { id: "luckyblockobby", title: "luckyblockobby", url: "../Games/luckyblockobby/index.html", desc: "Coming soon. Edit this description in main.js.", popular: true },
+
+  { id: "ninjaobby", title: "ninjaobby", url: "../Games/ninjaobby/index.html", desc: "Coming soon. Edit this description in main.js.", popular: true },
 ];
 let favoriteGamesList = JSON.parse(localStorage.getItem('nullx_favorites_arr')) || [];
 let contextTargetId = null;
